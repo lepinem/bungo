b94aecab5e8e897af2ecda43bbe16f160ef5f7eb
Stephen Schools <stephen.schools>
2017-09-06 16:33:51 -0400

    server info added

--- server.js
+++ server.js
@@ -1,2 +1,60 @@
-hi from ryan
-hi from matt
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const {getAllBeers, getBeersByBrewery, getBeersByType, getBeersByRating} = require("./dal");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const Beer = require('./models/Beer');
+const app = express();
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.redirect("/beers/home");
+});
+
+app.post("/beers/home", function(req, res) {
+  if (req.body.search) {
+    res.render("searchOptions");
+  } else {
+    res.render("addBeer");
+  }
+});
+
+app.post("/beers/searchOptions", function(req, res) {
+  let searchType = req.body.search;
+  if (searchType === "brewery") {
+    res.render("searchByBrewery");
+  } else if (searchType === "type") {
+    res.render("searchByType");
+  } else {
+    res.render("searchByRating");
+  }
+});
+
+app.post("beers/searchByBrewery", function(req, res) {
+  getBeersByBrewery(req.body.brewery).then(function(beers) {
+    res.render("list", {beers});
+  });
+});
+
+app.post("beers/searchByType", function(req, res) {
+  getBeersByBrewery(req.body.type).then(function(beers) {
+    res.render("list", {beers});
+  });
+});
+
+app.post("beers/searchByRating", function(req, res) {
+  getBeersByBrewery(req.body.rating).then(function(beers) {
+    res.render("list", {beers});
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Successfully started user directory application on: 3000");
+});
